e5e3714ca4fb8451e82f4c630355625276413d2c
J. Sihvola <jsihv@gmx.com>
2023-08-07 16:30:04 +0300

    jh7110 gpio

--- sys/riscv/starfive/jh7110_gpio.c
+++ sys/riscv/starfive/jh7110_gpio.c
@@ -1,8 +1,6 @@
 /*-
  * SPDX-License-Identifier: BSD-2-Clause
  *
- * Copyright (c) 2018 Emmanuel Vadot <manu@FreeBSD.org> (???)
- * Copyright (c) 2021 Soren Schmidt <sos@deepcore.dk>   (???)
  * Copyright (c) 2023 Jari Sihvola <jsihv@gmx.com>
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,8 +24,6 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-
-/* This is not yet modified for JH7110 */
 
 #include <sys/cdefs.h>
 __FBSDID("$FreeBSD$");
@@ -54,17 +50,23 @@ __FBSDID("$FreeBSD$");
 
 #include "gpio_if.h"
 
-#define JH7100_GPIO_PINS 64
+#define JH7110_GPIO_PINS 64
 
-#define GPIOEN          0x0
-#define GPIOE_0         0x28
-#define GPIOE_1         0x2c
-#define GPIO_DIN_LOW	0x48
-#define GPIO_DIN_HIGH	0x4c
-#define GP0_DOUT_CFG	0x50
-#define GP0_DOEN_CFG    0x54
+#define GPIOEN          0xdc
+#define GPIOE_0         0x100
+#define GPIOE_1         0x104
+#define GPIO_DIN_LOW	0x118
+#define GPIO_DIN_HIGH	0x11c
+#define GP0_DOUT_CFG	0x0
+#define GP0_DOEN_CFG    0x40
 
-struct jh7100_gpio_softc {
+#define ENABLE_MASK     0x3f
+#define DATA_OUT_MASK   0x7f
+#define ALIGN_4(_val)   (_val & ~3)
+#define MOD_4(_val)     (_val & 3)
+
+
+struct jh7110_gpio_softc {
 	device_t              dev; 
 	device_t              busdev;
 	struct mtx            mtx; 
@@ -73,26 +75,26 @@ struct jh7100_gpio_softc {
 };
 
 static struct ofw_compat_data compat_data[] = { 
-	{"starfive,jh7100-pinctrl", 1},
+	{"starfive,jh7110-sys-pinctrl", 1},
 	{NULL,             0} 
 };
 
-static struct resource_spec jh7100_gpio_spec[] = {
+static struct resource_spec jh7110_gpio_spec[] = {
 	{ SYS_RES_MEMORY,     0,      RF_ACTIVE },
 	{ -1, 0 } 
 };
 
-#define JH7100_GPIO_LOCK(_sc)               mtx_lock(&(_sc)->mtx)
-#define JH7100_GPIO_UNLOCK(_sc)             mtx_unlock(&(_sc)->mtx)
+#define JH7110_GPIO_LOCK(_sc)               mtx_lock(&(_sc)->mtx)
+#define JH7110_GPIO_UNLOCK(_sc)             mtx_unlock(&(_sc)->mtx)
 
-#define JH7100_GPIO_READ(sc, reg)           bus_read_4((sc)->res, (reg))
-#define JH7100_GPIO_WRITE(sc, reg, val)     bus_write_4((sc)->res, (reg), (val))
+#define JH7110_GPIO_READ(sc, reg)           bus_read_4((sc)->res, (reg))
+#define JH7110_GPIO_WRITE(sc, reg, val)     bus_write_4((sc)->res, (reg), (val))
 
 static device_t
-jh7100_gpio_get_bus(device_t dev)
+jh7110_gpio_get_bus(device_t dev)
 {
-	struct jh7100_gpio_softc *sc;
-	printf("jh7100_gpio_get_bus\n");
+	struct jh7110_gpio_softc *sc;
+	printf("jh7110_gpio_get_bus\n");
 
 	sc = device_get_softc(dev);
 
@@ -100,109 +102,119 @@ jh7100_gpio_get_bus(device_t dev)
 }
 
 static int
-jh7100_gpio_pin_max(device_t dev, int *maxpin)
+jh7110_gpio_pin_max(device_t dev, int *maxpin)
 {
-	*maxpin = JH7100_GPIO_PINS - 1;
+	*maxpin = JH7110_GPIO_PINS - 1;
 
 	return (0);
 }
 
 static int
-jh7100_gpio_pin_get(device_t dev, uint32_t pin, unsigned int *val)
+jh7110_gpio_pin_get(device_t dev, uint32_t pin, unsigned int *val)
 {
-	struct jh7100_gpio_softc *sc;
+	struct jh7110_gpio_softc *sc;
 	uint32_t reg;
-	printf("jh7100_gpio_get_get\n");
+	printf("jh7110_gpio_get_get\n");
 	
 	sc = device_get_softc(dev);
 
-	if (pin >= JH7100_GPIO_PINS)
+	if (pin >= JH7110_GPIO_PINS)
 		return (EINVAL);
 
-	JH7100_GPIO_LOCK(sc);
+	JH7110_GPIO_LOCK(sc);
 	if (pin < 32) {
-		reg = JH7100_GPIO_READ(sc, GPIO_DIN_LOW);
+		reg = JH7110_GPIO_READ(sc, GPIO_DIN_LOW);
 		*val = (reg >> pin) & 0x1;
 	}
 	else {
-		reg = JH7100_GPIO_READ(sc, GPIO_DIN_HIGH);
+		reg = JH7110_GPIO_READ(sc, GPIO_DIN_HIGH);
 		*val = (reg >> (pin - 32)) & 0x1;
 	}
-	JH7100_GPIO_UNLOCK(sc);
+	JH7110_GPIO_UNLOCK(sc);
 
 	return (0);
 }
 
 static int
-jh7100_gpio_pin_set(device_t dev, uint32_t pin, unsigned int value)
+jh7110_gpio_pin_set(device_t dev, uint32_t pin, unsigned int value)
 {
-	struct jh7100_gpio_softc *sc;
-	printf("jh7100_gpio_pin_set\n");
+	struct jh7110_gpio_softc *sc;
+	uint32_t reg;
+	printf("jh7110_gpio_pin_set\n");
 
 	sc = device_get_softc(dev);
 
-	if (pin >= JH7100_GPIO_PINS)
+	if (pin >= JH7110_GPIO_PINS)
 		return (EINVAL);
 
-	JH7100_GPIO_LOCK(sc);
-	JH7100_GPIO_WRITE(sc, GP0_DOUT_CFG + pin * 8, value);
-	JH7100_GPIO_UNLOCK(sc);
+	JH7110_GPIO_LOCK(sc);
+	reg = JH7110_GPIO_READ(sc, GP0_DOUT_CFG + ALIGN_4(pin));
+	reg &= ~(DATA_OUT_MASK << MOD_4(pin) * 8);
+	reg |= reg << MOD_4(pin) * 8;
+	JH7110_GPIO_WRITE(sc, GP0_DOUT_CFG + ALIGN_4(pin), value);
+	JH7110_GPIO_UNLOCK(sc);
 
 	return (0);
 }
 
 static int
-jh7100_gpio_pin_getflags(device_t dev, uint32_t pin, uint32_t *flags)
+jh7110_gpio_pin_getflags(device_t dev, uint32_t pin, uint32_t *flags)
 {
-	struct jh7100_gpio_softc *sc;
-	printf("jh7100_gpio_pin_getflags\n");
+	struct jh7110_gpio_softc *sc;
+	uint32_t reg;
+	printf("jh7110_gpio_pin_getflags\n");
 	
 	sc = device_get_softc(dev);
 
-	if (pin >= JH7100_GPIO_PINS)
+	if (pin >= JH7110_GPIO_PINS)
 		return (EINVAL);
 
 	/* Reading the direction */
-
-	JH7100_GPIO_LOCK(sc);
-	if (JH7100_GPIO_READ(sc, GP0_DOEN_CFG + pin * 8) & 0x1)
-		*flags |= GPIO_PIN_INPUT;
-	else
+	JH7110_GPIO_LOCK(sc);
+	reg = JH7110_GPIO_READ(sc, GP0_DOEN_CFG + ALIGN_4(pin));
+	if ((reg & ENABLE_MASK << MOD_4(pin) * 8) == 0)
 		*flags |= GPIO_PIN_OUTPUT;
-	JH7100_GPIO_UNLOCK(sc);
+	else 
+		*flags |= GPIO_PIN_INPUT;
+	JH7110_GPIO_UNLOCK(sc);
 	
 	return (0);
 }
 
 
 static int
-jh7100_gpio_pin_setflags(device_t dev, uint32_t pin, uint32_t flags)
+jh7110_gpio_pin_setflags(device_t dev, uint32_t pin, uint32_t flags)
 {
-	struct jh7100_gpio_softc *sc;
-	printf("jh7100_gpio_pin_setflags\n");
+	struct jh7110_gpio_softc *sc;
+	uint32_t reg;
+	printf("jh7110_gpio_pin_setflags\n");
 
 	sc = device_get_softc(dev);
 		
-	if (pin >= JH7100_GPIO_PINS)
+	if (pin >= JH7110_GPIO_PINS)
 		return (EINVAL);
 
 	/* Setting the direction, enable or disable output */
-
-	JH7100_GPIO_LOCK(sc);
+	JH7110_GPIO_LOCK(sc);
 	if (flags & GPIO_PIN_INPUT) {
-		JH7100_GPIO_WRITE(sc, GP0_DOEN_CFG + pin * 8, 0x1);
+		reg = JH7110_GPIO_READ(sc, GP0_DOUT_CFG + ALIGN_4(pin));
+		reg &= ~(ENABLE_MASK << MOD_4(pin) * 8);
+		reg |= 1 << MOD_4(pin) * 8;
+		JH7110_GPIO_WRITE(sc, GP0_DOUT_CFG + ALIGN_4(pin), reg);
 	}
-
 	else if (flags & GPIO_PIN_OUTPUT) {
-		JH7100_GPIO_WRITE(sc, GP0_DOEN_CFG + pin * 8, 0x0);
+		reg = JH7110_GPIO_READ(sc, GP0_DOUT_CFG + ALIGN_4(pin));
+		reg &= ~(ENABLE_MASK << MOD_4(pin) * 8);
+		reg |= 0 << MOD_4(pin) * 8;
+		JH7110_GPIO_WRITE(sc, GP0_DOUT_CFG + ALIGN_4(pin), reg);
 	}
-	JH7100_GPIO_UNLOCK(sc); 
+	JH7110_GPIO_UNLOCK(sc); 
 	
 	return (0);
 }
 
 static int
-jh7100_gpio_probe(device_t dev) 
+jh7110_gpio_probe(device_t dev) 
 {
 
 	if (!ofw_bus_status_okay(dev)) {
@@ -213,20 +225,20 @@ jh7100_gpio_probe(device_t dev)
 		return (ENXIO);
 	}
 
-	device_set_desc(dev, "jh7100 GPIO generator driver");
+	device_set_desc(dev, "jh7110 GPIO generator driver");
 	
 	return (BUS_PROBE_DEFAULT);
 }
 
 static int
-jh7100_gpio_detach(device_t dev)
+jh7110_gpio_detach(device_t dev)
 {
-	struct jh7100_gpio_softc *sc;
-	printf("jh7100_gpio_detach\n");
+	struct jh7110_gpio_softc *sc;
+	printf("jh7110_gpio_detach\n");
 	
 	sc = device_get_softc(dev);
 
-	bus_release_resources(dev, jh7100_gpio_spec, &sc->res);
+	bus_release_resources(dev, jh7110_gpio_spec, &sc->res);
 	gpiobus_detach_bus(dev);
 	mtx_destroy(&sc->mtx); 
 	
@@ -234,32 +246,27 @@ jh7100_gpio_detach(device_t dev)
 }
 
 static int
-jh7100_gpio_attach(device_t dev)
+jh7110_gpio_attach(device_t dev)
 {
-	struct jh7100_gpio_softc *sc;
-	phandle_t node;
+	struct jh7110_gpio_softc *sc;
 	int err;
 
-	printf("jh7100_gpio_attach\n");
+	printf("jh7110_gpio_attach\n");
 	sc = device_get_softc(dev);
 	sc->dev = dev;
 
-	node = ofw_bus_get_node(sc->dev);
-	if (!OF_hasprop(node, "gpio-controller"))
-		return (ENXIO);
-
 	mtx_init(&sc->mtx, device_get_nameunit(sc->dev), NULL, MTX_DEF);
 
-	if (bus_alloc_resources(dev, jh7100_gpio_spec, &sc->res)) { 
-		device_printf(dev, "could not allocate resources\n"); 
-		bus_release_resources(dev, jh7100_gpio_spec, &sc->res); 
+	if (bus_alloc_resources(dev, jh7110_gpio_spec, &sc->res)) { 
+		device_printf(dev, "Could not allocate resources\n"); 
+		bus_release_resources(dev, jh7110_gpio_spec, &sc->res); 
 		mtx_destroy(&sc->mtx); 
 		return (ENXIO); 
 	} 
 
 	if (clk_get_by_ofw_index(dev, 0, 0, &sc->clk) != 0) { 
 		device_printf(dev, "Cannot get clock\n"); 
-		jh7100_gpio_detach(dev);
+		jh7110_gpio_detach(dev);
 		return (ENXIO); 
 	}
 
@@ -267,54 +274,53 @@ jh7100_gpio_attach(device_t dev)
 	if (err != 0) { 
 		device_printf(dev, "Could not enable clock %s\n", 
 			      clk_get_name(sc->clk)); 
-		jh7100_gpio_detach(dev);
+		jh7110_gpio_detach(dev);
 		return (ENXIO); 
 	} 
 
 	sc->busdev = gpiobus_attach_bus(dev);
 	if (sc->busdev == NULL) {
 		device_printf(dev, "Cannot attach gpiobus\n");
-		jh7100_gpio_detach(dev);
+		jh7110_gpio_detach(dev);
 		return (ENXIO);
 	}
 
 	/* Reseting GPIO interrupts */
-	/* jh7100 datasheet [2021-4-21] errorneously claims 1 is to disable */
-	JH7100_GPIO_WRITE(sc, GPIOE_0, 0);
-	JH7100_GPIO_WRITE(sc, GPIOE_1, 0);
-	JH7100_GPIO_WRITE(sc, GPIOEN, 1);
+	JH7110_GPIO_WRITE(sc, GPIOE_0, 0);
+	JH7110_GPIO_WRITE(sc, GPIOE_1, 0);
+	JH7110_GPIO_WRITE(sc, GPIOEN, 1);
 	
 	return (0);
 }
 
 static phandle_t  
-jh7100_gpio_get_node(device_t bus, device_t dev) 
+jh7110_gpio_get_node(device_t bus, device_t dev) 
 {
 	return (ofw_bus_get_node(bus)); 
 }
 
-static device_method_t jh7100_gpio_methods[] = {
+static device_method_t jh7110_gpio_methods[] = {
         /* Device interface */
-	DEVMETHOD(device_probe,         jh7100_gpio_probe), 
-	DEVMETHOD(device_attach,        jh7100_gpio_attach),
-	DEVMETHOD(device_detach,        jh7100_gpio_detach),
+	DEVMETHOD(device_probe,         jh7110_gpio_probe), 
+	DEVMETHOD(device_attach,        jh7110_gpio_attach),
+	DEVMETHOD(device_detach,        jh7110_gpio_detach),
 
         /* GPIO protocol */
-	DEVMETHOD(gpio_get_bus,         jh7100_gpio_get_bus),
-	DEVMETHOD(gpio_pin_max,         jh7100_gpio_pin_max),
-	DEVMETHOD(gpio_pin_getflags,    jh7100_gpio_pin_getflags),
-	DEVMETHOD(gpio_pin_setflags,    jh7100_gpio_pin_setflags),
-	DEVMETHOD(gpio_pin_get,         jh7100_gpio_pin_get), 
-	DEVMETHOD(gpio_pin_set,         jh7100_gpio_pin_set), 
+	DEVMETHOD(gpio_get_bus,         jh7110_gpio_get_bus),
+	DEVMETHOD(gpio_pin_max,         jh7110_gpio_pin_max),
+	DEVMETHOD(gpio_pin_getflags,    jh7110_gpio_pin_getflags),
+	DEVMETHOD(gpio_pin_setflags,    jh7110_gpio_pin_setflags),
+	DEVMETHOD(gpio_pin_get,         jh7110_gpio_pin_get), 
+	DEVMETHOD(gpio_pin_set,         jh7110_gpio_pin_set), 
 
         /* ofw_bus interface */ 
-	DEVMETHOD(ofw_bus_get_node,     jh7100_gpio_get_node),
+	DEVMETHOD(ofw_bus_get_node,     jh7110_gpio_get_node),
 
 	DEVMETHOD_END 
 }; 
 
-DEFINE_CLASS_0(gpio, jh7100_gpio_driver, jh7100_gpio_methods,
-    sizeof(struct jh7100_gpio_softc));
-EARLY_DRIVER_MODULE(jh7100_gpio, simplebus, jh7100_gpio_driver, 0, 0,
+DEFINE_CLASS_0(gpio, jh7110_gpio_driver, jh7110_gpio_methods,
+    sizeof(struct jh7110_gpio_softc));
+EARLY_DRIVER_MODULE(jh7110_gpio, simplebus, jh7110_gpio_driver, 0, 0,
     BUS_PASS_INTERRUPT + BUS_PASS_ORDER_MIDDLE);
-MODULE_DEPEND(jh7100_gpio, gpiobus, 1, 1, 1);
+MODULE_DEPEND(jh7110_gpio, gpiobus, 1, 1, 1);
